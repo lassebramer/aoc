3dad336fabef826d22a92cfb18acf2c66dbd594c
Lasse Bramer Schmidt <lassebramer@gmail.com>
2024-12-09 09:57:51 +0100

    push old stuf

---
+++ 2024/1.go
@@ -0,0 +1,11 @@
+package day1
+
+import (
+	"github.com/pivovarit/aoc/util"
+)
+
+func main() {
+	input := util.ReadInput()
+	// fmt.Println(getSumCalibrationNumbers(output, true))
+	// fmt.Println(getSumCalibrationNumbers(output, false))
+}
